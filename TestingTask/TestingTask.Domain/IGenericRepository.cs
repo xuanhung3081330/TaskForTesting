64623f878e1ba35148f0c76021f4a248a73212ef
Xuan Hung <hung.nguyen@saleshub.vn>
2020-12-22 20:33:58 +0700

    Create Generic Repository and .gitignore file

---
+++ TestingTask/TestingTask.Domain/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingTask.Domain
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(object id);
+        Task InsertAsync(T obj);
+        Task UpdateAsync(T obj);
+        Task DeleteAsync(object id);
+        Task SaveAsync();
+    }
+}
